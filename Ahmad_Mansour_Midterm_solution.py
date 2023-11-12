99336676cbd00998c86eb49d990f7a51a3c4a63a
ahmad <madrid7.am79@gmail.com>
2023-11-12 18:28:37 +0200

    function validate url

--- Ahmad_Mansour_Midterm_solution.py
+++ Ahmad_Mansour_Midterm_solution.py
@@ -1,3 +1,14 @@
 import json
 import requests
 from bs4 import BeautifulSoup
+def validate_url(add_or_open):
+    try:
+        title=input("enter title ").strip()
+        url=input("enter url ").strip()
+        requests.head(url,timeout=15)
+    except requests.exceptions.RequestException:
+        return print("url invalide")
+    if add_or_open=="add_tab":
+        return add_tab(title,url)
+    if add_or_open=="open_nested_tab": 
+        return open_nested_tab(title,url)
