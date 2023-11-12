95249a6a98189a45cf248805a77772faaaffa136
ahmad <madrid7.am79@gmail.com>
2023-11-12 19:11:01 +0200

    display menu

--- Ahmad_Mansour_Midterm_solution.py
+++ Ahmad_Mansour_Midterm_solution.py
@@ -82,5 +82,16 @@ def import_tabs():
                 read=json.load(read_file)
                 print(read)
     except FileNotFoundError:
-        print("please enter the correct file name")            
+        print("please enter the correct file name")
+ def main():
+        print("Menu:")
+        print("1. Open Tab")
+        print("2. Close Tab")
+        print("3. Switch Tab")
+        print("4. Display All Tabs")
+        print("5. Open Nested Tab")
+        print("6. Clear All Tabs")
+        print("7. Save Tabs")
+        print("8. Import Tabs")
+        print("9. Exit")                    
             
